6274be91ebacdfc966078bdfcde6f057c8a849f1
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-07-17 14:51:48 +0200

    change in server.js, post and get in products/index.js

---
+++ src/routes/products/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const db = require("../../db")
+const product = require("../../../products.json") ///MOMENTANEO
+
+const router = express.Router()
+
+router.get("/:_id", async (req, res) => {
+    const response = await db.query('SELECT _id, name, description, brand, imageUrl, price FROM "product" WHERE _id = $1',
+        [req.params._id])
+
+    if (response.rowCount === 0)
+        return res.status(404).send("Not exist")
+
+    res.send(response.rows[0])
+})
+
+router.post("/", async (req, res) => {
+    const response = await db.query(`INSERT INTO "product" (_id, name, description, brand, imageUrl, price, category) 
+                                     Values ($1, $2, $3, $4, $5, $6, $7)
+                                     RETURNING *`,
+        [req.body._id, req.body.name, req.body.description, req.body.brand, req.body.imageUrl, req.body.price, req.body.category])
+
+    console.log(response)
+    res.send(response.rows[0])
+})
--- src/server.js
+++ src/server.js
@@ -3,19 +3,13 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const { request } = require("express")
 dotenv.config()
-const db = require("./database")
+//const db = require("./database")
+const productRouter = require("./routes/products")
 
 const server = express()
 server.use(cors())
 server.use(express.json())//////VEDERE SE POI SARA' NECESSARIO TENERLO DOPO AVER FATTO DEI TEST CON DEI JSON
 
-server.get("/", (req, res) => {
-    res.send("OK")
-})
-
-server.get("/testamazon", async (req, res) => {
-    const response = await db.query("SELECT 5+5")
-    res.send(response)
-})
+server.use("./products", productRouter)
 
 server.listen(process.env.PORT || 3458, () => console.log("Server is running")) 
